8bcd3e8f8f8a120e84f31ff479b322f7179cd14d
Vincent Thiberville <vincent.thiberville@intersec.com>
2016-01-07 20:54:16 +0100

    clang-tidy: add intersec module, tscope checker

---
+++ clang-tools-extra/clang-tidy/intersec/IntersecTidyModule.cpp
@@ -0,0 +1,49 @@
+//===--- IntersecTidyModule.cpp - clang-tidy --------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "../ClangTidy.h"
+#include "../ClangTidyModule.h"
+#include "../ClangTidyModuleRegistry.h"
+#include "../readability/BracesAroundStatementsCheck.h"
+#include "../readability/FunctionSizeCheck.h"
+#include "../readability/NamespaceCommentCheck.h"
+#include "../readability/RedundantSmartptrGetCheck.h"
+#include "TscopeCheck.cpp"
+
+using namespace clang::ast_matchers;
+
+namespace clang {
+namespace tidy {
+namespace intersec {
+
+class IntersecModule : public ClangTidyModule {
+public:
+  void addCheckFactories(ClangTidyCheckFactories &CheckFactories) override {
+    CheckFactories.registerCheck<TscopeCheck>(
+        "intersec-tscope");
+  }
+
+  ClangTidyOptions getModuleOptions() override {
+    ClangTidyOptions Options;
+    return Options;
+  }
+};
+
+// Register the IntersecTidyModule using this statically initialized variable.
+static ClangTidyModuleRegistry::Add<IntersecModule> X("intersec-module",
+                                                    "Adds Intersec lint checks.");
+
+} // namespace intersec
+
+// This anchor is used to force the linker to link in the generated object file
+// and thus register the IntersecModule.
+volatile int IntersecModuleAnchorSource = 0;
+
+} // namespace tidy
+} // namespace clang
---
+++ clang-tools-extra/clang-tidy/intersec/TscopeCheck.cpp
@@ -0,0 +1,63 @@
+//===--- TscopeCheck.cpp - clang-tidy--------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "TscopeCheck.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "llvm/Support/Regex.h"
+
+using namespace clang::ast_matchers;
+
+namespace clang {
+namespace tidy {
+
+void TscopeCheck::registerMatchers(MatchFinder *Finder) {
+  // creation of a new t stack
+  auto tpush = callExpr(callee(namedDecl(matchesName("mem_stack_push"))));
+
+  Finder->addMatcher(
+    callExpr(expr().bind("callee"),
+             // find call to a function needing a t_scope
+             callee(namedDecl(matchesName("::_*z?r?_?t_"))),
+             // inside a function definition
+             hasAncestor(functionDecl(decl().bind("caller"),
+                                      isDefinition())),
+             // but do not match if a new t_scope was created
+             unless(hasAncestor(compoundStmt(
+               anyOf(
+                 // direct call to mem_stack_push
+                 has(tpush),
+                 // "t_scope;" instruction
+                 has(declStmt(has(varDecl(has(tpush)))))
+               )
+             )))
+    ), this);
+}
+
+void TscopeCheck::check(const MatchFinder::MatchResult &Result) {
+  auto called = Result.Nodes.getNodeAs<CallExpr>("callee");
+  auto caller = Result.Nodes.getNodeAs<FunctionDecl>("caller");
+  auto caller_name = caller->getName();
+
+  // if the function is not inside a t_scope, return an error
+  // Matches all variants:
+  //  - with leading underscores
+  //  - with "z_" prefix
+  //  - with "r" or "r_" prefix, if both a r_pool and t_scope are used.
+  if (!llvm::Regex("^_*z?r?_?t_").match(caller_name)
+  &&  !caller_name.equals("ipool"))
+  {
+    diag(called->getLocStart(), "function '%0' is called by function '%1' "
+        "but is missing a t_scope.")
+      << called->getDirectCallee()->getName() << caller->getName();
+  }
+}
+
+} // namespace tidy
+} // namespace clang
